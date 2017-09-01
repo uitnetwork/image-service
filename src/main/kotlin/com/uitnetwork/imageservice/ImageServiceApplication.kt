05c21e43b6ca5e5aece751b17257c21aaff677e7
Ninh Doan <uitnetwork@gmail.com>
2017-09-01 18:47:31 +0800

    First commit

---
+++ src/main/kotlin/com/uitnetwork/imageservice/ImageServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.uitnetwork.imageservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ImageServiceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ImageServiceApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/uitnetwork/imageservice/ImageServiceApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.uitnetwork.imageservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ImageServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
